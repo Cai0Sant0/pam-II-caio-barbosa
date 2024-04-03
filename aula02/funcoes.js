72f8adaeeb78bcbf4aebe492878bd12bee8ba744
Caio Barbosa <caiomontebarbosa104@gmail.com>
2024-04-03 08:32:10 -0300

    Segunda aula

--- aula02/funcoes.js
+++ aula02/funcoes.js
@@ -10,3 +10,32 @@ console.log( typeof(num1));
 console.log( typeof(msg));
 
 console.log(`O número é ${num1}`);
+
+function somar (){
+    let n1 = 2;
+    let n2 = 5;
+    console.log("O resultado é: ", n1 + n2);
+}
+
+somar();
+
+function somarComParametros(n1,n2){
+    let resultado = `O resultado de ${n1} mais ${n2} é ${n1+n2}`;
+    console.log(resultado);
+}
+
+somarComParametros(10,20);
+
+let teste =(n)=>{
+    let resultado = n%2;
+
+    if(resultado == 1){
+        return "Número impar";
+    }
+
+    return "Número par";
+}
+
+console.log(teste(2));
+console.log(teste(3));
+// teste(1)  Esse não executa uma visualização
