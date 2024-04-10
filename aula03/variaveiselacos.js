ca7278657e7a1cceaaec409e38248680d7243f1d
Caio Barbosa <caiomontebarbosa104@gmail.com>
2024-04-10 08:31:00 -0300

    Aula03-laços e variáveis

---
+++ aula03/variaveiselacos.js
@@ -0,0 +1,23 @@
+let n = 2;
+let t = "oi";
+
+console.log(typeof(n));
+console.log(typeof(t));
+
+let a = [0, 1, 1, 2, 3, 5, 8, 13, 21];
+
+console.log(typeof(a));
+
+let o = {nome: "Caio", idade: "18"};//JSON -> Javascript Object Notation
+
+console.log(typeof(o));
+
+console.log(a);
+console.log(a[6]);
+
+a.forEach((valor)=>{
+    console.log(`Elemento : ${valor}`);
+});
+
+console.log(o);
+console.log(o.nome, "tem", o.idade);
